b254ccfb9aef3cdd1c13da864e4f2d9ad5811d92
pia <piabindal1156@gmail.com>
2026-04-07 22:23:27 +0530

    added basic product dao structure

---
+++ ProductDAO.java
@@ -0,0 +1,30 @@
+import java.sql.*;
+import java.util.*;
+
+
+class ProductDAO {
+    // we'll figure logic later, just writing structure for now
+    public void addProduct(Product p) {
+        
+    }
+
+    public void updateProduct(Product p) {
+
+    }
+
+    public void searchProduct(String name) {
+        
+    }
+
+    public List<Product> getAllProducts() {
+        return new ArrayList<>();
+    }
+
+    public void deleteProduct(int id) {
+        
+    }
+
+    public void updateStock(int id, int stock) {
+       
+    }
+}
