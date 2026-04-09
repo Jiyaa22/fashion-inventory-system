af348a80970a63360ddbca2eecca19c08d899ae0
jiyaa <jiyaadramc@gmail.com>
2026-04-09 19:10:32 +0530

    improved inventory service and added delete and stock update methods

--- InventoryService.java
+++ InventoryService.java
@@ -4,15 +4,28 @@ class InventoryService {
 
     ProductDAO dao = new ProductDAO();
 
+    // improved method to view products
     public void viewProducts() {
         List<Product> list = dao.getAllProducts();
 
-        for (int i = 0; i < list.size(); i++) {
-            list.get(i).display();
+        if (list.isEmpty()) {
+            System.out.println("No products available.");
+            return;
+        }
+        for (Product p : list) {
+            p.display();
         }
     }
 
     public void addProduct(Product p) {
         dao.addProduct(p);
+    }
+
+    public void deleteProduct(int id) {
+        dao.deleteProduct(id);
+    }
+
+    public void updateStock(int id, int stock) {
+        dao.updateStock(id, stock);
     }
 }
